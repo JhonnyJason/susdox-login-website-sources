d0a2308a29b31f95e4d0251f62eedebd0cf160e8
Lenard Frommelt <lenny09918050@gmail.com>
2022-12-21 14:37:23 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,7 +1,8 @@
 module.exports = {
     getDependencies: () => {
         return {
-	        "thingy-byte-utils":"^0.0.7"
+            "libsodium-wrappers": "^0.7.10",
+            "thingy-byte-utils":"^0.0.7"
         }
     }
 }
