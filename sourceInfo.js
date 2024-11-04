4570662ff59b75fe0e875fe1657bf2e725e80414
Lenard Frommelt <lenny09918050@gmail.com>
2024-11-04 14:17:36 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,7 +1,7 @@
 module.exports = {
     getDependencies: () => {
         return {
-            "libsodium-wrappers-sumo": "^0.7.13",
+            "libsodium-wrappers-sumo": "^0.7.15",
             "navhandler": "^0.0.4",
             "thingy-byte-utils":"^0.0.8"
         }
